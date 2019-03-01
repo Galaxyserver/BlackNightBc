ed2c76421557363c5d68178a594ca9f22cd60e0a
ShaherAhly2005 <46113872+Galaxyserver@users.noreply.github.com>
2019-03-01 19:45:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
 
 client.on("message", message => {
 
-    if (message.content.startsWith("Bbbc")) {
+    if (message.content.startsWith("Rbbc")) {
                  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
   var argresult = args.join(' ');
